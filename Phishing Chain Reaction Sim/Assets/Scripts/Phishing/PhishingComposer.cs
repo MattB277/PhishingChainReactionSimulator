918e00f5dca89393a9274ae9fc1860dca5352ff3
Matthew Ballantyne <2774408b@student.gla.ac.uk>
2026-01-04 19:56:43 +0000

    Progress on #21 Implemented level progression, UI metadata, win/loss calculation and handling.

--- Phishing Chain Reaction Sim/Assets/Scripts/Phishing/PhishingComposer.cs
+++ Phishing Chain Reaction Sim/Assets/Scripts/Phishing/PhishingComposer.cs
@@ -12,7 +12,18 @@ public class PhishingComposer : MonoBehaviour
     [Header("Stat UI References")]
     [SerializeField] private Slider successSlider;
     [SerializeField] private Slider suspicionSlider;
+
+    [Header("Result UI")]
+    [SerializeField] private GameObject resultPanel;
+    [SerializeField] private TextMeshProUGUI resultText;
+    [SerializeField] private Button nextLevelButton; // only appears on win
+    [SerializeField] private Button closeResultButton; // only appears on fail
     
+    void Start()
+    {
+        // hide result panel
+        if(resultPanel) resultPanel.SetActive(false);
+    }
     public void OnModulesChanged()
     {
         var currentModules = dropZone.GetCurrentModuleData();
@@ -32,8 +43,50 @@ public class PhishingComposer : MonoBehaviour
     
     public void SendPost()
     {
-        // NOT YET IMPLEMENTED
-        Debug.Log("SEND MESSAGE CLICKED");
+        var currentModules = dropZone.GetCurrentModuleData();
+
+        if (currentModules.Count == 0)
+        {
+            Debug.Log("PhishingComposer: Cannot send empty messages");
+            return;
+        }
+
+        // calculate final score
+        float finalScorePercent = Mathf.Clamp01(successSlider.value - suspicionSlider.value) * 100f;
+        // get threshold from ScenarioManager
+        float scenarioThreshold = ScenarioManager.Instance.GetCurrentTreshold();
+
+        bool isWin = finalScorePercent >= scenarioThreshold;
+
+        ShowResult(isWin, finalScorePercent, scenarioThreshold);
+    }
+
+    private void ShowResult(bool isWin, float score, float threshold)
+    {
+        resultPanel.SetActive(true);
+
+        if (isWin)
+        {
+            resultText.text = "Placeholder Success";
+            nextLevelButton.gameObject.SetActive(true);
+            closeResultButton.gameObject.SetActive(false);
+        }
+        else
+        {
+            resultText.text = "Placeholder Fail";
+            nextLevelButton.gameObject.SetActive(false);
+            closeResultButton.gameObject.SetActive(true);
+        }
+    }
+
+    private void AdvanceLevel()
+    {
+        resultPanel.SetActive(false);
+        ClearComposer();
+        int nextLevelIdx = ScenarioManager.Instance.CurrentLevelIdx + 1;
+        ScenarioManager.Instance.LoadScenario(nextLevelIdx);
+        palette.PopulatePallete(nextLevelIdx);
+        Debug.Log($"Finished {ScenarioManager.Instance.CurrentLevelIdx}. Advanced to level index {nextLevelIdx}");
     }
 
     // Update is called once per frame
